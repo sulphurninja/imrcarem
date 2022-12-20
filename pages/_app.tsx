1e050690eaf9d7728cf4d7c9645133634a1c98a0
sulphurninja <sulphurninja@gmail.com>
2022-12-20 05:48:23 +0530

    first commit

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,6 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
-import { Provider } from "react-redux";
-import { store } from "../redux/store";
+
 
 
 
@@ -9,10 +8,10 @@ import { store } from "../redux/store";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <Provider store={store}>
+
 
   <Component {...pageProps} />
 
-    </Provider>
+
   )
 }
