1a3dbbe784a19dbff022fafb4de47f5f397d962e
sulphurninja <sulphurninja@gmail.com>
2022-12-20 05:54:22 +0530

    first commit

--- utils/fetchCategories.ts
+++ utils/fetchCategories.ts
@@ -1,3 +1,5 @@
+import { Category } from "../typings";
+
 export const fetchCategories = async () => {
   const res = await fetch(
     `http://localhost:3000/api/getCategories`
--- utils/fetchProducts.ts
+++ utils/fetchProducts.ts
@@ -1,3 +1,5 @@
+import { Product } from "../typings";
+
 export const fetchProducts = async () => {
   const res = await fetch(
     `http://localhost:3000/api/getProducts`
