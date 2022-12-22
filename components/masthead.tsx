5d873f3b6efa9ca7f8b1dcc1c8739f30c3231d17
sulphurninja <sulphurninja@gmail.com>
2022-12-23 02:35:45 +0530

    working  commit

--- components/masthead.tsx
+++ components/masthead.tsx
@@ -21,7 +21,7 @@ const Masthead: React.FC = () => {
         <div className='p-12 font-bold z-10 text-white  drop-shadow-[0_5px_3px_rgba(0,0,0,0.4)] text-center flex-1 flex items-center justify-center flex-col'>
             <h1 className='mb-6 text-4xl xl:text-5xl '>imrcare</h1>
             <h2 className='mb-2 text-2xl xl:text-3xl tracking-tight'>
-                <span>Mobile repair,</span> <span>done right.</span>
+                <span className='text-amber-400'>Mobile repair,</span> <span>done right.</span>
             </h2>
         </div>
         <div className='flex-grow-0 pb-20 md:pb-10 transition-all duration-1000
